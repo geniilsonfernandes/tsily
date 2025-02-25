672971bb17ab37c72f1c2c0ebaed4cffa624ae7b
gedev <geniilsonfernandes@gmail.com>
2025-02-25 14:39:45 -0300

    Refactor ThemedText and ThemedView components to support customizable color names

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,25 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps } from "react-native";
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { Colors } from "@/constants/Colors";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  colorName?: keyof typeof Colors.light;
 };
 
-export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+export function ThemedView({
+  style,
+  lightColor,
+  colorName = "background",
+  darkColor,
+  ...otherProps
+}: ThemedViewProps) {
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    colorName
+  );
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
