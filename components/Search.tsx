3b344e546bb57a8e22a4b72e6c67a983c8eebb11
gedev <geniilsonfernandes@gmail.com>
2025-02-25 13:51:32 -0300

    Add Search component

---
+++ components/Search.tsx
@@ -0,0 +1,58 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Feather } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import { ThemedView } from "./ThemedView";
+
+export const Search = () => {
+  const backgroundColor = useThemeColor({}, "background.1");
+  const textColor = useThemeColor({}, "text");
+  return (
+    <View style={[styles.container, { backgroundColor }]}>
+      <View style={styles.icon}>
+        <Feather name="search" size={16} color={textColor} />
+      </View>
+      <TextInput
+        placeholder="Search"
+        placeholderTextColor={textColor}
+        style={[styles.input, { color: textColor }]}
+      />
+      <TouchableOpacity>
+        <ThemedView colorName="background.2" style={styles.leftContainer}>
+          <Feather name="filter" size={16} color={textColor} />
+        </ThemedView>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 8,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    height: 48,
+  },
+  icon: {
+    height: 32,
+    width: 32,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+   
+  },
+  input: {
+    flex: 1,
+   
+  },
+  leftContainer: {
+    width: 32,
+    height: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+});
